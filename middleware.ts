8c8f81d7c1f8fb757e01d551b5ee45618f004c35
Aryan <aryanpatil1406@gmail.com>
2026-02-03 22:10:12 +0530

    Refactor download handler in Home component for improved readability and performance

--- middleware.ts
+++ middleware.ts
@@ -2,10 +2,10 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 import { NextResponse } from 'next/server';
 
 const isPublicRoute = createRouteMatcher([
-    "/sign-in",
-    "/sign-up",
+    "/sign-in(.*)",
+    "/sign-up(.*)",
     "/",
-    "/home"
+    "/api/webhooks(.*)"
 ])
 const isPublicApiRoute = createRouteMatcher([
     "/api/videos"
@@ -15,13 +15,8 @@ const isPublicApiRoute = createRouteMatcher([
 export default clerkMiddleware((auth, req) => {
     const {userId} = auth();
     const currentUrl = new URL(req.url)
-     const isAccessingDashboard = currentUrl.pathname === "/home"
-     const isApiRequest = currentUrl.pathname.startsWith("/api")
+    const isApiRequest = currentUrl.pathname.startsWith("/api")
 
-     // If user is logged in and accessing a public route but not the dashboard
-    if(userId && isPublicRoute(req) && !isAccessingDashboard) {
-        return NextResponse.redirect(new URL("/home", req.url))
-    }
     //not logged in
     if(!userId){
         // If user is not logged in and trying to access a protected route
